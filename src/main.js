6341e3728b722ac5bb225b523ceec7c5bf73cbc2
msyfls123 <m18771058712@live.com>
2016-08-29 19:59:49 +0800

    Dragula

--- src/main.js
+++ src/main.js
@@ -4,17 +4,40 @@ document.write("<div id=\"app\"></div>")
 
 
 import React,{Component} from 'react'
-import { render } from 'react-dom'
+import { render,findDOMNode } from 'react-dom'
+import Dragula from 'react-dragula'
 
 let root = document.getElementById('app')
+let cursor = {
+  left:0,
+  top:0
+}
+const options = {
+  copy: false
+}
 
 class Root extends Component {
   render() {
-    return (
-      <div>
-        XJW，我好喜欢你啊!
-      </div>
-    )
+    return (<div className='container'>
+          <div data-id="1">1. XJW, I like U</div>
+          <div data-id="2">2. XJW, I like U</div>
+          <div data-id="3">3. XJW, I like U</div>
+          <div data-id="4">4. XJW, I like U</div>
+          <div data-id="5">5. XJW, I like U</div>
+          <div data-id="6">6. XJW, I like U</div>
+          <div data-id="7">7. XJW, I like U</div>
+    </div>)
+  }
+  componentDidMount() {
+    var container = findDOMNode(this);
+    let drake = Dragula([container],options);
+    drake.on('drop',function(el, target, source, sibdivng) {
+      if(sibdivng){
+        console.log(el.getAttribute("data-id"), sibdivng.getAttribute("data-id"))
+      }else{
+        console.log(el.getAttribute("data-id"), null)
+      }
+    })
   }
 }
 
